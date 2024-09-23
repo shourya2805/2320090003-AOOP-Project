1c164c82ab3c5f102e67782ddb0c127b2a2e2bae
shourya2805 <korrapatishourya@gmail.com>
2024-09-23 11:05:36 +0530

    Delete CreditCardPaymentFactory.java

--- CreditCardPaymentFactory.java
+++
@@ -1,8 +0,0 @@
-package week3;
-
-class CreditCardPaymentFactory implements PaymentMethodFactory {
-    @Override
-    public PaymentMethod createPaymentMethod() {
-        return new CreditCardPayment();
-    }
-}
